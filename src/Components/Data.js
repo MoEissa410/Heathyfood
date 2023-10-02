76b67ed743c25f5e14fd9a6002d33741ffeead07
moeissa411 <74250349+moeissa411@users.noreply.github.com>
2023-10-02 19:26:08 +0300

    final

--- src/Components/Data.js
+++ src/Components/Data.js
@@ -50,7 +50,7 @@ const Items = [
     imgSrc:
       "https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Fburger1.png?alt=media&token=319dfbe9-462b-46ea-8f38-6ca7a20319e0",
     name: "Burger Bistro",
-    ratings: 5,
+    ratings: 4.6,
     price: "7.5",
   },
   {
